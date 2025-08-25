b2b144c7a3ff21d7d1a96697ddf39f113071a36e
matt <hey@mattt.space>
2025-08-25 16:45:45 -0700

    fix: todo

--- components/forms/actions/newsletter.tsx
+++ components/forms/actions/newsletter.tsx
@@ -5,17 +5,17 @@ import { render } from "@react-email/render";
 import Welcome from "../../mail/welcome";
 
 export async function subscribe(formData: FormData) {
-  const email = formData.get("email");
-  if (!email || typeof email !== "string") return;
+	const email = formData.get("email");
+	if (!email || typeof email !== "string") return;
 
-  const body = await render(<Welcome baseURL="https://upcoming.chiliwap.ca" />);
+	const body = await render(<Welcome baseURL="https://chiliwap.ca" />);
 
-  const plunk = new Plunk(process.env.PLUNK_API_KEY!);
-  await plunk.emails.send({
-    name: "Chiliwap News",
-    to: email,
-    subject: "Welcome to Chiliwap News",
-    subscribed: true,
-    body,
-  });
+	const plunk = new Plunk(process.env.PLUNK_API_KEY!);
+	await plunk.emails.send({
+		name: "Chiliwap News",
+		to: email,
+		subject: "Welcome to Chiliwap News",
+		subscribed: true,
+		body,
+	});
 }
