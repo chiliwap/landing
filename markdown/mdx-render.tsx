8d7c2f4bec7953a782678ab39fbaebbcde3c3aca
matt <hey@mattt.space>
2025-08-25 14:36:29 -0700

    fix: mdx-render routing

--- markdown/mdx-render.tsx
+++ markdown/mdx-render.tsx
@@ -3,7 +3,7 @@
 import dynamic from "next/dynamic";
 
 export default function Renderer(props: { slug: string }) {
-  const Post = dynamic(() => import(`@/content/${props.slug}.mdx`), {
+  const Post = dynamic(() => import(`@/markdown/${props.slug}.mdx`), {
     ssr: false,
   });
 
